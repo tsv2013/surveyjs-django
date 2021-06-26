5317829370846f8bda37b5064bed51d0f9a863de
TSV <tsv2013@users.noreply.github.com>
2021-06-26 12:47:39 +0300

    Started surveys list view

--- service/urls.py
+++ service/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+from service.models import Survey
+from django.urls import include, path
 from service import views
+
+survey_list_view = views.SurveyListView.as_view(
+    # :5 limits the results to the five most recent
+    queryset=Survey.objects.order_by("-id")[:5],
+    context_object_name="survey_list",
+    template_name="service/surveys.html",
+)
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
+    path("surveys/", survey_list_view, name="surveys"),
     path("api/", include("service.api")),
 ]
--- service/views.py
+++ service/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Survey
 
 # Create your views here.
 
@@ -7,3 +9,12 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "service/home.html")
+
+
+class SurveyListView(ListView):
+    """Renders the surveys page, with a list of all surveys."""
+    model = Survey
+
+    def get_context_data(self, **kwargs):
+        context = super(SurveyListView, self).get_context_data(**kwargs)
+        return context
